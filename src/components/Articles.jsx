2b534f56084c0a041e122434b4740f78ed587c12
yuridevweb <yuridevweb@gmail.com>
2022-05-19 07:25:49 +0100

    bad path  error handling

--- src/components/Articles.jsx
+++ src/components/Articles.jsx
@@ -1,103 +1,109 @@
-import { useEffect, useState } from 'react'
-import { getArticles } from '../utils/api'
-import { Row } from 'react-bootstrap'
-import { useParams } from 'react-router-dom'
-import { Link, useSearchParams } from 'react-router-dom'
-import Card from 'react-bootstrap/Card'
-import Col from 'react-bootstrap/Col'
-import Spinner from 'react-bootstrap/Spinner'
+import { useEffect, useState } from "react";
+import { getArticles } from "../utils/api";
+import { Row } from "react-bootstrap";
+import { useParams } from "react-router-dom";
+import { Link, useSearchParams } from "react-router-dom";
+import Card from "react-bootstrap/Card";
+import Col from "react-bootstrap/Col";
+import Spinner from "react-bootstrap/Spinner";
 
 const Articles = () => {
-  const [searchParams, setSearchParams] = useSearchParams()
-  const [articles, setArticles] = useState([])
-  const [isLoading, setIsLoading] = useState(true)
-  const [order, setOrder] = useState('desc')
-  const [sort, setSort] = useState('created_at')
-  const { topic } = useParams()
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [articles, setArticles] = useState([]);
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [order, setOrder] = useState("desc");
+  const [sort, setSort] = useState("created_at");
+  const { topic } = useParams();
   useEffect(() => {
-    setIsLoading(true)
-    getArticles(topic).then((articlesFromApi) => {
-      setArticles(articlesFromApi)
-      setIsLoading(false)
-    })
-  }, [topic])
+    setError(null);
+    setIsLoading(true);
+    getArticles(topic)
+      .then((articlesFromApi) => {
+        setArticles(articlesFromApi);
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        setError("Sorry, topic doesn't exist...");
+      });
+  }, [topic]);
 
   const handleSortBy = (e) => {
-    let params = { sort_by: e.target.value, order: order }
-    setSearchParams(params)
-    setSort(params.sort_by)
-    setIsLoading(true)
+    let params = { sort_by: e.target.value, order: order };
+    setSearchParams(params);
+    setSort(params.sort_by);
+    setIsLoading(true);
     getArticles(topic, params.sort_by, order).then((articlesFromApi) => {
-      setArticles(articlesFromApi)
-      setIsLoading(false)
-    })
-  }
+      setArticles(articlesFromApi);
+      setIsLoading(false);
+    });
+  };
 
   const handleOrderBy = (e) => {
-    let params = { sort_by: sort, order_by: e.target.value }
-    setSearchParams(params)
-    setOrder(params.order_by)
-    setIsLoading(true)
+    let params = { sort_by: sort, order_by: e.target.value };
+    setSearchParams(params);
+    setOrder(params.order_by);
+    setIsLoading(true);
     getArticles(topic, sort, params.order_by).then((articlesFromApi) => {
-      setArticles(articlesFromApi)
-      setIsLoading(false)
-    })
-  }
-
+      setArticles(articlesFromApi);
+      setIsLoading(false);
+    });
+  };
+  if (error) return <p>{error}</p>;
   return (
     <main>
-      {!topic && <h2 className='pt-3'>All topics</h2>}
-      <h2 className='capitalize pt-3'>{topic}</h2>
-      <div className='container'>
-        <div className='row'>
-          <div className='col-6'>
-            <label htmlFor='sort-by-select'>Sort:</label>
+      {!topic && <h2 className="pt-3">All topics</h2>}
+      <h2 className="capitalize pt-3">{topic}</h2>
+      <div className="container">
+        <div className="row">
+          <div className="col-6">
+            <label htmlFor="sort-by-select">Sort:</label>
           </div>
-          <div className='col-6'>
-            <label htmlFor='order-by-select'>Order:</label>
+          <div className="col-6">
+            <label htmlFor="order-by-select">Order:</label>
           </div>
         </div>
-        <div className='row'>
-          <div className='col-6'>
+        <div className="row">
+          <div className="col-6">
             <select
-              className='form-select'
-              id='sort-by-select'
+              className="form-select"
+              id="sort-by-select"
               onChange={handleSortBy}
             >
-              <option value='created_at'>By Date</option>
-              <option value='votes'>By Votes</option>
-              <option value='comment_count'>By Comments</option>
+              <option value="created_at">By Date</option>
+              <option value="votes">By Votes</option>
+              <option value="comment_count">By Comments</option>
             </select>
           </div>
 
-          <div className='col-6'>
+          <div className="col-6">
             <select
-              className='form-select'
-              id='order-by-select'
+              className="form-select"
+              id="order-by-select"
               onChange={handleOrderBy}
             >
-              <option value='desc'>Descending</option>
-              <option value='asc'>Ascending</option>
+              <option value="desc">Descending</option>
+              <option value="asc">Ascending</option>
             </select>
           </div>
         </div>
       </div>
 
       {isLoading ? (
-        <Spinner animation='border' />
+        <Spinner animation="border" />
       ) : (
         <ul>
           {articles.map((article) => {
             return (
-              <li className='articles-list' key={article.article_id}>
+              <li className="articles-list" key={article.article_id}>
                 <Link to={`/articles/${article.article_id}`}>
-                  <Card className='text-center'>
+                  <Card className="text-center">
                     <Card.Body>
-                      <Card.Title className='article-title'>
+                      <Card.Title className="article-title">
                         {article.title}
                       </Card.Title>
                     </Card.Body>
-                    <Card.Footer className='text-muted'>
+                    <Card.Footer className="text-muted">
                       <Row>
                         <Col sm={6}>
                           Posted on: {article.created_at.slice(0, 10)}
@@ -109,12 +115,12 @@ const Articles = () => {
                   </Card>
                 </Link>
               </li>
-            )
+            );
           })}
         </ul>
       )}
     </main>
-  )
-}
+  );
+};
 
-export default Articles
+export default Articles;
